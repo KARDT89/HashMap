6144cbd103c7f7bee752811ca86e60056a69dbe5
dt89mint <dt89@mint.com>
2025-07-07 02:09:37 +0530

    hashmap done

--- HashMap.js
+++ HashMap.js
@@ -4,9 +4,10 @@ class HashMap {
         this.bucket = new Array(this.capacity).fill(null);
         this.load_factor = load_factor;
         this.totalStoredKeys = 0;
+        this._rehashing = false;
     }
 
-    // recieve bucket index
+    // Generate an index in the bucket for a given key
     hash(key) {
         let hashCode = 0;
 
@@ -18,13 +19,14 @@ class HashMap {
         return hashCode;
     }
 
+    // Add or update a key-value pair
     set(key, value) {
         let bucketIndex = this.hash(key);
 
         if (this.bucket[bucketIndex] === null) {
             this.bucket[bucketIndex] = [[`${key}`, value]];
             this.totalStoredKeys++;
-            this.validate()
+            this.validate();
             return `Successfully Added -> [${key}: ${value}]`;
         } else {
             const bucketIndexLength = this.bucket[bucketIndex].length;
@@ -38,10 +40,11 @@ class HashMap {
             this.bucket[bucketIndex].push([`${key}`, value]);
         }
         this.totalStoredKeys++;
-        this.validate()
+        this.validate();
         return `Successfully Added -> [${key}: ${value}]`;
     }
 
+    // Retrieve a value for a given key
     get(key) {
         const bucketIndex = this.hash(key);
         if (this.bucket[bucketIndex]) {
@@ -55,6 +58,8 @@ class HashMap {
         }
         return null;
     }
+
+    // Check if a key exists
     has(key) {
         const bucketIndex = this.hash(key);
         if (this.bucket[bucketIndex]) {
@@ -69,6 +74,7 @@ class HashMap {
         return false;
     }
 
+    // Remove a key-value pair
     remove(key) {
         const bucketIndex = this.hash(key);
         if (this.bucket[bucketIndex]) {
@@ -82,7 +88,7 @@ class HashMap {
                         this.bucket[bucketIndex].splice(i, 1);
                     }
                     this.totalStoredKeys--;
-                    this.validate()
+                    this.validate();
                     return true;
                 }
             }
@@ -90,17 +96,19 @@ class HashMap {
         return false;
     }
 
+    // Return the total number of keys stored
     length() {
         return this.totalStoredKeys;
     }
 
+    // Clear all entries from the map
     clear() {
         this.bucket = new Array(this.capacity).fill(null);
-        this.load_factor = 0;
         this.totalStoredKeys = 0;
         return "Successfully Removed All Entries";
     }
 
+    // Return all keys in the map
     keys() {
         let result = [];
         for (let i = 0; i < this.bucket.length; i++) {
@@ -113,6 +121,7 @@ class HashMap {
         return result;
     }
 
+    // Return all values in the map
     values() {
         let result = [];
         for (let i = 0; i < this.bucket.length; i++) {
@@ -124,6 +133,7 @@ class HashMap {
         }
         return result;
     }
+    // Return all entries (key-value pairs)
     entries() {
         let result = [];
         for (let i = 0; i < this.bucket.length; i++) {
@@ -136,17 +146,31 @@ class HashMap {
         return result;
     }
 
+    // Resize the map if load factor is exceeded
     validate() {
-        if (this.totalStoredKeys / this.capacity > this.load_factor) {
-            this.capacity *= 2;
-            this.reHash()
-        } 
-        // todo make it dynamic
+        if (this._rehashing) return; // break the infinite loop
+
+        let newCapacity = 16;
+        while (true) {
+            if (this.totalStoredKeys / newCapacity <= this.load_factor) {
+                this.capacity = newCapacity;
+                break;
+            }
+            newCapacity *= 2;
+        }
+        if (newCapacity !== this.capacity) {
+            this.capacity = newCapacity;
+            this._rehashing = true;
+            this.reHash();
+            this._rehashing = false;
+        }
     }
+    // Rebuild the entire hash map when resized
     reHash() {
         const entries = this.entries();
         this.bucket = new Array(this.capacity).fill(null);
         this.totalStoredKeys = 0;
+
         for (let [key, value] of entries) {
             this.set(key, value);
         }
@@ -168,6 +192,5 @@ test.set("kite", "pink");
 test.set("lion", "golden");
 test.set("lion2", "golden");
 
-
-console.log(test.remove("elephant"));
+// console.log(test.remove("elephant"));
 console.log(test.capacity);
